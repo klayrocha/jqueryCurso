6d773ee6d372997b496821b1e473582d877dad46
Francis Klay Rocha <klayrocha@gmail.com>
2018-05-24 11:58:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// EXEMPLO 01
+//$( document ).ready(function(){
+//    alert("Iniciou no ready");
+//});
+
+// EXEMPLO 02
+//$(function(){
+//  alert("Iniciou no ready nova forma");
+//});
+
+// EXEMPLO 03
+//$( document ).ready(funcaoInicio);
+//$(funcaoInicio());
+//$(funcaoInicio);
+//function funcaoInicio(){
+//    alert("Iniciou no ready chamando funcao");
+//}
+
+// EXEMPLO 04
+//var $K =  jQuery.noConflict();
+//$K(funcaoInicio);
+//$K(alert('Com o novo nome'));
+
+// EXEMPLO 05
+//jQuery(document).ready(function(){
+//    alert(' Funcionando Nativamente...');
+//});
+
+// EXEMPLO 06
+$(function(){
+    //$("button"); pelo nome da tag
+    alert($("#botao1")); // pelo id
+    alert($(".botao")); // pelo classe
+    alert($("li.botao")); // apenas a li que tem a classe botão
+})
